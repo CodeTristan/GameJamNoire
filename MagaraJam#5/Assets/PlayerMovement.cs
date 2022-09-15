78cea51fd934360227dd39ec81435ce158c3b942
ftpixel <atahanhuyuk@gmail.com>
2022-09-15 21:34:44 +0300

    SMol change to test git hub

--- MagaraJam#5/Assets/PlayerMovement.cs
+++ MagaraJam#5/Assets/PlayerMovement.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  void Start()
+  {
+    //Testing github
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void Update()
+  {
+
+  }
 }
