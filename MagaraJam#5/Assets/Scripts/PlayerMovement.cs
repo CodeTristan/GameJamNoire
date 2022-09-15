bf95a6bd617cb0378a09316268a331d36b5af067
ftpixel <atahanhuyuk@gmail.com>
2022-09-15 22:24:26 +0300

    Movement

--- MagaraJam#5/Assets/Scripts/PlayerMovement.cs
+++ MagaraJam#5/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+  public float speed;
+
+  Rigidbody rb;
+  Vector3 dir;
+
   void Start()
   {
-    //Testing github
+    rb = this.gameObject.GetComponent<Rigidbody>();
   }
 
   void Update()
   {
+    dir = new Vector3(0,0,0);
+
+    if(Input.GetKey(KeyCode.RightArrow))
+      dir.x += 1;
+    if(Input.GetKey(KeyCode.LeftArrow))
+      dir.x -= 1;
+    if(Input.GetKey(KeyCode.UpArrow))
+      dir.z += 1;
+    if(Input.GetKey(KeyCode.DownArrow))
+      dir.z -= 1;
+
+    rb.velocity = dir * speed;
 
   }
 }
